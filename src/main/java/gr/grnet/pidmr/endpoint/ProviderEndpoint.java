ebc6a425fdf14b05d0608bb46bfbf78e7c0855e6
fbasios <fbasios@admin.grnet.gr>
2023-05-12 15:49:10 +0300

    Enable CORS in api

--- src/main/java/gr/grnet/pidmr/endpoint/ProviderEndpoint.java
+++ src/main/java/gr/grnet/pidmr/endpoint/ProviderEndpoint.java
@@ -20,7 +20,6 @@ import javax.validation.constraints.NotEmpty;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
