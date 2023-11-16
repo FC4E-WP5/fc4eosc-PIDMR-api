859ff7be8ca961cd83453bd0f3fdca84c27f4074
fbasios <fbasios@admin.grnet.gr>
2023-11-16 10:29:35 +0200

    Return the healthcheck of the api

---
+++ src/main/java/org/grnet/pidmr/client/AAIProxyClient.java
@@ -0,0 +1,21 @@
+package org.grnet.pidmr.client;
+
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+@Path("/.well-known")
+@RegisterRestClient
+@ApplicationScoped
+/**
+ * This HTTP client communicates with Eosc Infrastructure Proxy.
+ */
+public interface AAIProxyClient {
+
+    @GET
+    @Path("/openid-configuration")
+    Response configurations();
+}
---
+++ src/main/java/org/grnet/pidmr/health/AAIProxyHealthCheck.java
@@ -0,0 +1,44 @@
+package org.grnet.pidmr.health;
+
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.HealthCheckResponseBuilder;
+import org.eclipse.microprofile.health.Readiness;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.grnet.pidmr.client.AAIProxyClient;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.ws.rs.core.Response;
+
+@Readiness
+@ApplicationScoped
+public class AAIProxyHealthCheck implements HealthCheck {
+
+    @Inject
+    @RestClient
+    AAIProxyClient AAIProxyClient;
+
+
+    @Override
+    public HealthCheckResponse call() {
+
+        HealthCheckResponseBuilder responseBuilder = HealthCheckResponse.named("Authentication and Authorization Infrastructure health check");
+
+        try {
+            Response response = AAIProxyClient.configurations();
+
+            if(response.getStatus() == Response.Status.OK.getStatusCode()){
+                responseBuilder.up();
+            } else {
+                responseBuilder.down().withData("error", "Cannot access the Authentication and Authorization Infrastructure.");
+            }
+
+        } catch (Exception e){
+
+            responseBuilder.down().withData("error", "Cannot access the Authentication and Authorization Infrastructure.");
+        }
+
+        return responseBuilder.build();
+    }
+}
