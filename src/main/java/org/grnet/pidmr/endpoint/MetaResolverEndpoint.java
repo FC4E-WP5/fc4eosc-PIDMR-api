349b5bebb19c4e2d38d61805b85461d3b804893c
fbasios <fbasios@admin.grnet.gr>
2024-02-19 12:09:40 +0200

    Batch request for identify

--- src/main/java/org/grnet/pidmr/endpoint/MetaResolverEndpoint.java
+++ src/main/java/org/grnet/pidmr/endpoint/MetaResolverEndpoint.java
@@ -40,7 +40,7 @@ public class MetaResolverEndpoint {
     @Inject
     MetaresolverService metaresolverService;
 
-    @ConfigProperty(name = "max.pid.list.size")
+    @ConfigProperty(name = "max.resolution.pid.list.size")
     int maxPidListSize;
 
     public MetaResolverEndpoint(MetaresolverService metaresolverService) {
@@ -168,5 +168,5 @@ public class MetaResolverEndpoint {
         });
 
         return Response.status(Response.Status.OK).entity(response).build();
-        }
+    }
 }
